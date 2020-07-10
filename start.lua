679d3c6f9241d9743c5f4efaa79e7d9385b6964a
ALIKHV <64853762+ALIKHV@users.noreply.github.com>
2020-07-10 11:44:13 +0300

    Update start.lua

--- start.lua
+++ start.lua
@@ -1,16 +1,16 @@
-sudo_KENAE = dofile("sudo.lua")
+sudo_ALIKHV = dofile("sudo.lua")
 https = require("ssl.https")
 JSON = dofile("./libs/JSON.lua")
-local KENAE_dev = io.open("KENAE_online.lua")
-if KENAE_dev then
-KENAE_on = {string.match(KENAE_dev:read('*all'), "^(.*)/(%d+)")}
-local KENAE_file = io.open("sudo.lua", 'w')
-KENAE_file:write("token = '" ..KENAE_on[1].."'\n\nsudo_add = "..KENAE_on[2].."" )
-KENAE_file:close()
-https.request("https://api.telegram.org/bot"..KENAE_on[1].."/sendMessage?chat_id="..KENAE_on[2].."&text=Bot_KENAE_is_start_new")
+local ALIKHV_dev = io.open("ALIKHV_online.lua")
+if ALIKHV_dev then
+ALIKHV_on = {string.match(ALIKHV_dev:read('*all'), "^(.*)/(%d+)")}
+local ALIKHV_file = io.open("sudo.lua", 'w')
+ALIKHV_file:write("token = '" ..ALIKHV_on[1].."'\n\nsudo_add = "..ALIKHV_on[2].."" )
+ALIKHV_file:close()
+https.request("https://api.telegram.org/bot"..ALIKHV_on[1].."/sendMessage?chat_id="..ALIKHV_on[2].."&text=Bot_ALIKHV_is_start_new")
 os.execute('cd .. && rm -rf .telegram-cli')
-os.execute('rm -rf KENAE_online.lua')  
-os.execute('./tg -s ./KENAE.lua $@ --bot='..KENAE_on[1])
+os.execute('rm -rf ALIKHV_online.lua')  
+os.execute('./tg -s ./ALIKHV.lua $@ --bot='..ALIKHV_on[1])
 end
 function chack(tokenCk)
 local getme = "https://api.telegram.org/bot" ..tokenCk.. '/getme'
@@ -20,12 +20,12 @@ if data.ok == true then
 print("\27[31m✓ تم\27[m \27[1;34m»»ارسل ايدي المبرمج الاساسي««\27[m")
 local sudo_send  = io.read()
 print("\27[31m✓ تم\27[m")
-local KENAE_file = io.open("sudo.lua", 'w')
-KENAE_file:write("token = '" ..tokenCk.."'\n\nsudo_add = "..sudo_send.."" )
-KENAE_file:close()
+local ALIKHV_file = io.open("sudo.lua", 'w')
+ALIKHV_file:write("token = '" ..tokenCk.."'\n\nsudo_add = "..sudo_send.."" )
+ALIKHV_file:close()
 os.execute('cd .. && rm -fr .telegram-cli')
 os.execute('cd && rm -fr .telegram-cli')
-os.execute('./tg -s ./KENAE.lua $@ --bot='..tokenCk)
+os.execute('./tg -s ./ALIKHV.lua $@ --bot='..tokenCk)
 else
 print("\27[31m»»التوكن غير صحيح , اعد ارسال التوكن««\27[m")
 local token_send = io.read()
@@ -40,12 +40,12 @@ chack(token_send)
 else 
 os.execute('cd .. && rm -fr .telegram-cli')
 os.execute('cd && rm -fr .telegram-cli')
-sudo_KENAE = dofile("sudo.lua")
+sudo_ALIKHV = dofile("sudo.lua")
 local getme = "https://api.telegram.org/bot" ..token.. '/getme'
 local req = https.request(getme)
 local data = JSON:decode(req)
 if data.ok == true then
-os.execute('./tg -s ./KENAE.lua $@ --bot='..token)
+os.execute('./tg -s ./ALIKHV.lua $@ --bot='..token)
 else
 print("\27[31mالتوكن غير صيح , اعد ارسال التوكن««\27[m")
 local token_send = io.read()
